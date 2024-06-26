f280b7d1beabd172a8d104c1b8956580f16f3b51
Navya Mallidi <mallidinavya635@gmail.com>
2024-06-26 15:40:26 +0530

    server

---
+++ models/todo.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const TodoSchema = new mongoose.Schema({
+    task: String,
+    done :{
+        type:Boolean,
+        default:false
+    }
+})
+
+export const TodoModel = mongoose.model("todos", TodoSchema)
